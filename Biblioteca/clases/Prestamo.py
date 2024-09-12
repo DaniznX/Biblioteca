7769c152cf44d6029a0dd852a02d37e8d7d1291a
Daniel Castillo <danye.castillor15@gmail.com>
2024-09-12 14:31:10 -0300

    Actualizar Prestamo.py

--- Biblioteca/clases/Prestamo.py
+++ Biblioteca/clases/Prestamo.py
@@ -1,6 +1,6 @@
 class prestamo:
     def __init__(self, Isbn, fecha, Nusuario):
         self.Isbn = Isbn
-        self.fecha = fecha
+        self.Fecha = Fecha
         self.Nusuario = Nusuario
         
